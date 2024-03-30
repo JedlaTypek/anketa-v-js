8b91e5f38ba595c9780d69fe953f00a43f4a7420
BuildTools <unconfigured@null.spigotmc.org>
2024-03-30 12:19:06 +0000

    Odesílání dat z formuláře do backendu

--- app.js
+++ app.js
@@ -8,34 +8,68 @@ app.set('view engine', 'ejs')
 
 // nastavení složky se statickými soubory (přílohy - jpg, css, mp4, ...)
 app.use(express.static('public'))
-
-app.use(bodyParser.urlencoded({ extended: true}))
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 // routa - domovská stránka webu
 app.get('/', function (req, res) {
-    fs.readFile('people.json', (err, people) => {
-        if(err) throw err;
-        people = JSON.parse(people);
-        fs.readFile('balances.json', (err, balances) => {
-            if(err) throw err;
-            balances = JSON.parse(balances);
-            if(balances.length != people.length){
+    getBalances((err, people) => {
+        if (err) {
+            console.error('Chyba při získávání zůstatků:', err);
+            res.status(500).send('Nastala chyba při získávání zůstatků');
+            return;
+        }
+        res.render('index', { people, year: new Date().getFullYear(), title: "Zápis informací do profilů"});
+    });
+})
+
+app.post("/submit", (req, res) => {
+    console.log(req.body);
+    getBalances((err, people) => {
+        if (err) {
+            console.error('Chyba při získávání zůstatků:', err);
+            res.status(500).send('Nastala chyba při získávání zůstatků');
+            return;
+        }
+        res.render('index', { people, year: new Date().getFullYear(), title: "Zápis informací do profilů"});
+    });
+  });
+
+  function getBalances(callback) {
+    fs.readFile('people.json', (err, peopleData) => {
+        if (err) {
+            callback(err);
+            return;
+        }
+        fs.readFile('balances.json', (err, balancesData) => {
+            if (err) {
+                callback(err);
+                return;
+            }
+            let people = JSON.parse(peopleData);
+            let balances = JSON.parse(balancesData);
+            if (balances.length != people.length) {
                 balances = updateBalances(people, balances);
                 console.log(balances);
                 fs.writeFile('balances.json', JSON.stringify(balances, null, 2), (err) => {
-                    if (err) throw err;
+                    if (err) {
+                        callback(err);
+                        return;
+                    }
                 });
             }
-            res.render('index', {people: balances, year: new Date().getFullYear()});
-        })
-    })
-})
+            // Zavolání callback funkce s výsledky
+            callback(null, balances);
+        });
+    });
+}
 
 function updateBalances(people, balances) {
     people.forEach(person => {
         const found = balances.find(balance => balance.name === person);
         if (!found) {
-            balances.push({ name: person, money: 0, power: 0, experience: 0});
+            balances.push({ id: balances.length ,name: person, money: 0, power: 0, experience: 0});
         }
     });
     return balances;
--- public/js/script.js
+++ public/js/script.js
@@ -1,11 +1,40 @@
-allMoney.addEventListener('input', (input) => {
-    console.log(input.data);
+function findChecked(){
+    let checkboxes = document.querySelectorAll('[id$="Check"]');
+    let checked = []
+    checkboxes.forEach((checkbox) => {
+        if(checkbox.checked) checked.push(checkbox);
+    })
+    return checked;
+}
+
+function updateProfileCards(){
+    checked = findChecked();
+    checked.forEach((checkbox) => {
+        let id = parseInt(checkbox.id);
+        document.getElementById(id+'Money').value = allMoney.value;
+        document.getElementById(id+'Power'). value = allPower.value;
+        document.getElementById(id+'Experience').value = allExperience.value;
+    })
+}
+
+function validate(){
+
+}
+
+allMoney.addEventListener('input', () => {
+    updateProfileCards();
 })
 
-allPower.addEventListener('input', (input) => {
-    console.log(input.data);
+allPower.addEventListener('input', () => {
+    updateProfileCards();
 })
 
-allExperience.addEventListener('input', (input) => {
-    console.log(input.data);
+allExperience.addEventListener('input', () => {
+    updateProfileCards();
 })
+
+oznacVse.addEventListener('change', () => {
+    let checkboxes = document.querySelectorAll('[id$="Check"]');
+    checkboxes.forEach(checkbox => checkbox.checked = oznacVse.value === 'true');
+    oznacVse.value = oznacVse.value === 'true' ? 'false' : 'true';
+});
